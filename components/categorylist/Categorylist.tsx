b7c53590d15ce1b5cd902dc74b3aa7c7736a79e8
Aleksei <alexey.frolov174@gmail.com>
2025-01-28 18:07:59 +0100

    featured + cats

--- components/categorylist/Categorylist.tsx
+++ components/categorylist/Categorylist.tsx
@@ -1,6 +1,92 @@
 import React from "react";
 import styles from "./categorylist.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Categorylist() {
-  return <div className={styles.container}>Cat List</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.food}`}
+        >
+          <Image
+            src="/food.png"
+            alt="cuisine"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          cuisine
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.culture}`}
+        >
+          <Image
+            src="/balkanculture.png"
+            alt="culture"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          culture
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.architecture}`}
+        >
+          <Image
+            src="/arch.png"
+            alt="archimg"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          architeture
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.people}`}
+        >
+          <Image
+            src="/people.png"
+            alt="people"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          people
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.history}`}
+        >
+          <Image
+            src="/history.png"
+            alt="history"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          history
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.sights}`}
+        >
+          <Image
+            src="/sights.png"
+            alt="sights"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          sightseeings
+        </Link>
+      </div>
+    </div>
+  );
 }
--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -2,5 +2,27 @@ import React from "react";
 import styles from "./featured.module.css";
 
 export default function Featured() {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        {" "}
+        <b>Hey, there!</b> Discover my Balkan Experience
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <img className={styles.image} src="/000045.JPG" alt="title-image" />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            doloremque minus atque, similique culpa quidem ducimus ad ipsa fugit
+            labore. Modi rem iusto temporibus quasi dicta quae maxime
+            repudiandae nemo?
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 }
